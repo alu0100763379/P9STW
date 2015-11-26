40130bb38e71c31cd66ede0e46cdef36f462c1d2
Alejandro Ravelo <alu0100763379@ull.edu.es>
2015-11-26 19:25:28 +0000

    Creada la clase SeleccionMultiple

---
+++ models/seleccion_multiple_model.js
@@ -0,0 +1,23 @@
+var Pregunta = require('./pregunta_model.js');
+
+function SeleccionMultiple(prgt, opciones){
+  Pregunta.call(this, prgt);
+  this.opciones = opciones;
+  for(var i = 0; i<opciones.length;i++){
+        this.view += "<input type='checkbox' name='respuesta' value='"+options[i]+"'>"+options[i]+"<br>";
+      }
+}
+
+
+SeleccionMultiple.prototype.get_prgt_type = function(){
+  return this.type;
+}
+
+SeleccionMultiple.prototype.get_prgt = function(){
+  return this.prgt;
+}
+
+SeleccionMultiple.prototype = new Pregunta();
+SeleccionMultiple.prototype.constructor = SeleccionMultiple;
+
+module.exports = SeleccionMultiple;
